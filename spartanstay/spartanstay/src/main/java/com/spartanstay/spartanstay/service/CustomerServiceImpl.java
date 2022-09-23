560c28c74cc5651b58583821bd2b23ab9000dfa6
vanessaucey <vanessa.vega@sjsu.edu>
2022-09-22 20:21:56 -0700

    Backend created the response messages to receive user information.

--- spartanstay/spartanstay/src/main/java/com/spartanstay/spartanstay/service/CustomerServiceImpl.java
+++ spartanstay/spartanstay/src/main/java/com/spartanstay/spartanstay/service/CustomerServiceImpl.java
@@ -1,4 +1,24 @@
 package com.spartanstay.spartanstay.service;
 
-public class CustomerServiceImpl {
+import com.spartanstay.spartanstay.model.Customer;
+import com.spartanstay.spartanstay.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepo.save(customer);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepo.findAll();
+    }
+
+    @Autowired
+    private CustomerRepository customerRepo;
 }
