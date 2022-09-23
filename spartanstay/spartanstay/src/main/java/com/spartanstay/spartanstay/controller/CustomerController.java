3ef43663771a88601f907f11a0c8e4392515b739
vanessaucey <vanessa.vega@sjsu.edu>
2022-09-22 20:21:56 -0700

    Backend created the response messages to receive user information.

--- spartanstay/spartanstay/src/main/java/com/spartanstay/spartanstay/controller/CustomerController.java
+++ spartanstay/spartanstay/src/main/java/com/spartanstay/spartanstay/controller/CustomerController.java
@@ -1,4 +1,28 @@
 package com.spartanstay.spartanstay.controller;
 
+import com.spartanstay.spartanstay.model.Customer;
+import com.spartanstay.spartanstay.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// This is where the endpoints go relevant to customers (login/logout)
+@RestController
+@RequestMapping("/credentials")
+@CrossOrigin
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Customer customer){
+       customerService.saveCustomer(customer);
+       return  "New customer is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Customer> list(){
+        return customerService.getAllCustomers();
+    }
 }
