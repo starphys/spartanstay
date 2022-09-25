0665f87f3834c5393afed89ec435b9cb12bf90e4
elizabethhillman <elizabethhillman0@gmail.com>
2022-09-25 12:34:03 -0700

    back end testing framework (junit)

---
+++ spartanstay/spartanstay/src/test/java/com/spartanstay/spartanstay/model/CustomerTest.java
@@ -0,0 +1,100 @@
+package com.spartanstay.spartanstay.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    /**
+     * This method will make sure all testing methods are uniform when they begin
+     */
+    @BeforeEach
+    void setUp() {
+        //examples of things to do
+        //currentUserEmail = spartanstay@gmail.com
+        //currentUserName = "";
+        //addUser(liz, hillman, liz.h@gmail.com, spartanstay);
+    }
+
+    /**
+     * This method will make sure all testing methods are uniform when they begin, and making sure no testing data is saved in main file
+     */
+    @AfterEach
+    void tearDown() {
+        //example
+        //removeUser(liz.h@gmail.com);
+    }
+
+    @Test
+    void setId() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void setEmail() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void setPassword() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void setFirstName() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void setLastName() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void getId() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void getEmail() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void getPassword() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void getFirstName() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+
+    @Test
+    void getLastName() {
+        setUp();
+        //testing code
+        tearDown();
+    }
+}
