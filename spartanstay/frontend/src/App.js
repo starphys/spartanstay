5a518eda3e5d5ccde26e7b38603ddae03761b45d
lovepreet uppal <lovepreet.uppal@sjsu.edu>
2022-10-06 19:46:10 -0700

    Sign-up complete along with navbar

--- spartanstay/frontend/src/App.js
+++ spartanstay/frontend/src/App.js
@@ -1,28 +1,22 @@
-//import logo from './logo.svg';
-import logo from './images/logo-1.png';
-
-import './App.css';
+import Navbar from "./Navbar"
+import SignUp from "./pages/SignUp"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt='logo'/>
-        <p>
-            <h1>Welcome to SpartanStay</h1>
-            
-        </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          About US
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
