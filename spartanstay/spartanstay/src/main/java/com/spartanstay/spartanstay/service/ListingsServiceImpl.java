80723db884b397995bddba19f7b52760c80cef86
Evan Astle <starphys@gmail.com>
2022-10-14 19:45:59 -0700

    Leverage external API to deliver listings, locations, and details to the backend. (#6)
    * Added parameters for checkin/checkout dates.
    
    Achieved a request from using listings API
    
    * Parameterized listings function, moved to service
    
    * Added function to get destinationId from location string
    
    * Listings function now returns only list of results
    
    * getDetails endpoint created with working functions, some cleanup
    
    Co-authored-by: vanessaucey <vanessa.vega@sjsu.edu>

---
+++ spartanstay/spartanstay/src/main/java/com/spartanstay/spartanstay/service/ListingsServiceImpl.java
@@ -0,0 +1,102 @@
+package com.spartanstay.spartanstay.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.json.JSONObject;
+
+public class ListingsServiceImpl implements ListingsService{
+
+    @Override
+    public String getListings(String destId, String checkIn, String checkOut, String sortOrder, String adults) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://hotels4.p.rapidapi.com/properties/list?destinationId="+ destId +
+                        "&pageNumber=1&pageSize=25" +
+                        "&checkIn="+checkIn+
+                        "&checkOut="+checkOut+
+                        "&adults1=" + adults +
+                        "&sortOrder=" + sortOrder +
+                        "&locale=en_US&currency=USD"))
+                .header("X-RapidAPI-Key", Secrets.API_KEY)
+                .header("X-RapidAPI-Host", "hotels4.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(response.body());
+
+        if(response != null) {
+            JSONObject json = new JSONObject((response.body()));
+            return json.getJSONObject("data").getJSONObject("body")
+                    .getJSONObject("searchResults").getJSONArray("results").toString();
+        }
+        return "{No response from listings search.}";
+
+    }
+
+    @Override
+    public String getLocationID(String destination) {
+        destination = destination.replaceAll(" ", "%20");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://hotels4.p.rapidapi.com/locations/v2/search?query=" + destination +
+                        "&locale=en_US&currency=USD"))
+                .header("X-RapidAPI-Key", Secrets.API_KEY)
+                .header("X-RapidAPI-Host", "hotels4.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(response != null) {
+            JSONObject json = new JSONObject(response.body());
+            return json.getJSONArray("suggestions").getJSONObject(0)
+                    .getJSONArray("entities").getJSONObject(0).getString("destinationId");
+        }
+        return "{No response from location search.}";
+    }
+
+    @Override
+    public String getDetails(String id, String checkIn, String checkOut, String adults) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://hotels4.p.rapidapi.com/properties/get-details?id="+ id +
+                        "&checkIn="+checkIn+
+                        "&checkOut="+checkOut+
+                        "&adults1=" + adults +
+                        "&locale=en_US&currency=USD"))
+                .header("X-RapidAPI-Key", Secrets.API_KEY)
+                .header("X-RapidAPI-Host", "hotels4.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(response.body());
+
+        if(response != null) {
+            return response.body();
+        }
+        return "{No response from details search.}";
+
+
+    }
+}
