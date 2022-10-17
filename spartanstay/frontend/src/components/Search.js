cb6e73107b6c8b51c4657d25af2f77d06480cbaf
Evan Astle <starphys@gmail.com>
2022-10-16 21:24:13 -0700

    Add trim function to city parameter

--- spartanstay/frontend/src/components/Search.js
+++ spartanstay/frontend/src/components/Search.js
@@ -9,10 +9,11 @@ function Search()
         e.preventDefault()
         const listing={destination:"San Jose", checkIn: "2022-12-20",checkOut: "2022-12-21",order: "PRICE", numAdults: 1}
         console.log(listing)
-        fetch(`http://localhost:8080/listings/rooms?destination=${encodeURIComponent(city)}&checkIn=2022-12-20&checkOut=2022-12-21&order=PRICE&numAdults=1`).then((response)=>{
+        fetch(`http://localhost:8080/listings/rooms?destination=${encodeURIComponent(city.trim())}&checkIn=2022-12-20&checkOut=2022-12-21&order=PRICE&numAdults=1`).then((response)=>{
         return response.json()
       }).then(data => {setResults(data); return data}).then(data => console.log(data))
     }
+    
     return (
       <div>
         <input type='text' value={city} placeholder="San Jose" onChange={(e) => {setCity(e.target.value)}}/>
