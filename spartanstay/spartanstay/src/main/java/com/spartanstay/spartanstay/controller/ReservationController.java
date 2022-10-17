0da6a2525882551b7fa23a35559aeaffab099b86
Andrew T <truong.a2002@gmail.com>
2022-10-16 17:19:32 -0700

    Added implementation to check if reservation already exists

--- spartanstay/spartanstay/src/main/java/com/spartanstay/spartanstay/controller/ReservationController.java
+++ spartanstay/spartanstay/src/main/java/com/spartanstay/spartanstay/controller/ReservationController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/reservation")
@@ -16,13 +19,26 @@ public class ReservationController {
 
     @PostMapping("/add")
     public String add(@RequestBody Reservation reservation){
-        System.out.println(reservation.getSpecialReq());
-        reservationService.saveReservation(reservation);
-        return "Reservation saved";
+        if (checkReservation(reservation) == true) {
+            return "Reservation already exists";
+        }
+        else {
+            System.out.println(reservation.getSpecialReq());
+            reservationService.saveReservation(reservation);
+            return "Reservation saved";
+        }
     }
 
     @GetMapping("/getAll")
     public List<Reservation> getAllReservations() {
         return reservationService.getAllReservations();
     }
+
+    public boolean checkReservation(Reservation reservation) {
+        List <Reservation> list = getAllReservations();
+        Map<Integer, Reservation> map =
+                list.stream().collect(Collectors.toMap(Reservation::getId, Function.identity()));
+        return map.containsKey(reservation.getId());
+    }
+
 }
