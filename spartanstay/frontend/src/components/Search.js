f2442739dc3ef9fb704fd1f6f4cdf2d21287f4fa
Evan Astle <starphys@gmail.com>
2022-10-16 21:12:04 -0700

    Searching by city name

--- spartanstay/frontend/src/components/Search.js
+++ spartanstay/frontend/src/components/Search.js
@@ -3,26 +3,23 @@ import {useState} from "react"
 function Search()
 {
   const [results, setResults] = useState(null)
+  const [city, setCity] = useState("")
 
     const handleClick=(e)=>{
         e.preventDefault()
         const listing={destination:"San Jose", checkIn: "2022-12-20",checkOut: "2022-12-21",order: "PRICE", numAdults: 1}
         console.log(listing)
-        fetch("http://localhost:8080/listings/rooms?destination=san%20jose&checkIn=2022-12-20&checkOut=2022-12-21&order=PRICE&numAdults=1").then((response)=>{
+        fetch(`http://localhost:8080/listings/rooms?destination=${encodeURIComponent(city)}&checkIn=2022-12-20&checkOut=2022-12-21&order=PRICE&numAdults=1`).then((response)=>{
         return response.json()
       }).then(data => {setResults(data); return data}).then(data => console.log(data))
     }
-    if(results != null) {
-      return (
-        <div>
-          <button onClick = {handleClick}>Search</button>
-          {results.map(result => (<p>{result.name}</p>))}
-        </div>
-      );
-    }
-    else {
-      return <button onClick = {handleClick}>Search</button>
-    }
+    return (
+      <div>
+        <input type='text' value={city} placeholder="San Jose" onChange={(e) => {setCity(e.target.value)}}/>
+        <button onClick = {handleClick}>Search</button>
+        {results != null ? results.map(result => (<p>{result.name}  {result.ratePlan.price.current}</p>)) : ""}
+      </div>
+    );
 }
 
 export default Search
