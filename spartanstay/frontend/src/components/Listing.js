75018ceddd3ffbd041afbedefa17d778b261bade
Evan Astle <starphys@gmail.com>
2022-10-20 21:01:48 -0700

    Options selector working

--- spartanstay/frontend/src/components/Listing.js
+++ spartanstay/frontend/src/components/Listing.js
@@ -1,9 +1,9 @@
 
 function Listing ({listing}) {
-
+//{listing.ratePlan.price.current}
     return (
         <div>
-            {listing.name} {listing.ratePlan.price.current}<br />
+            {listing.name} {listing.ratePlan ? listing.ratePlan.price.current : "Priceless"}<br />
             <img src={listing.optimizedThumbUrls.srpDesktop} alt="img" /><br /> 
             {listing.starRating}/5, {listing.address.streetAddress}, {listing.address.locality} {listing.address.region}, {listing.address.countryName} <br />  <br /> 
         </div>
--- spartanstay/frontend/src/components/Search.js
+++ spartanstay/frontend/src/components/Search.js
@@ -10,13 +10,14 @@ function Search()
   const [city, setCity] = useState("San Jose")
   const [startDate, setStartDate] = useState(today)
   const [endDate, setEndDate] = useState(today)
+  const [order, setOrder] = useState("PRICE")
 
     const handleClick=(e)=>{
         e.preventDefault()
         fetch(`http://localhost:8080/listings/rooms?destination=${encodeURIComponent(city.trim())}`+
         `&checkIn=${startDate}`+
         `&checkOut=${endDate}`+
-        `&order=PRICE`+
+        `&order=${order}`+
         `&numAdults=1`)
         .then((response)=>{
         return response.json()
@@ -29,6 +30,11 @@ function Search()
 
         <input type='date' value={startDate} min={today} onChange={(e) => {setStartDate(e.target.value); if(e.target.value > endDate) {setEndDate(e.target.value)}}}/>
         <input type='date' value={endDate} min={startDate} onChange={(e) => {setEndDate(e.target.value)}}/>
+
+        <select onChange={e => setOrder(e.target.value)}>
+          <option value="PRICE">Price: Low to High</option>
+          <option value="BEST_SELLER">Best</option>
+        </select>
 
         <button onClick = {handleClick}>Search</button>
         {results != null ? results.map(result => (<Listing listing={result} key={i++}/>))
