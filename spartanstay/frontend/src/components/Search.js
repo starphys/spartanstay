38b331d08260d0c08266e78cc65784fd31ec4e87
Evan Astle <starphys@gmail.com>
2022-10-23 14:29:58 -0700

    Added bad styling just to make this more cohesive

--- spartanstay/frontend/src/components/Search.js
+++ spartanstay/frontend/src/components/Search.js
@@ -1,5 +1,6 @@
 import {useState} from "react"
 import Results from "./Results"
+import "../SignUp.css"
 
 function Search()
 {
@@ -25,19 +26,32 @@ function Search()
     }
     
     return (
-      <div>
+      <div className="SignUp">
+        <div className="SignUpForm">
+          <label>Destination</label>
         <input type='text' placeholder="San Jose" onChange={(e) => {setCity(e.target.value)}}/>
 
+        <label>Dates</label>
         <input type='date' value={startDate} min={today} onChange={(e) => {setStartDate(e.target.value); if(e.target.value > endDate) {setEndDate(e.target.value)}}}/>
         <input type='date' value={endDate} min={startDate} onChange={(e) => {setEndDate(e.target.value)}}/>
 
+        <label>Sort Order</label>
         <select onChange={e => setOrder(e.target.value)}>
           <option value="PRICE">Price: Low to High</option>
           <option value="PRICE_HIGHEST_FIRST">Price: High to Low</option>
           <option value="BEST_SELLER">Best</option>
         </select>
+        <label>Number of Guests</label>
         <select onChange={e => setAdults(e.target.value)}>
           <option value={1}>Adults</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5+</option>
+        </select>
+        <select>
+          <option value={1}>Children</option>
           <option value={1}>1</option>
           <option value={2}>2</option>
           <option value={3}>3</option>
@@ -47,6 +61,7 @@ function Search()
 
 
         <button onClick = {handleClick}>Search</button>
+        </div>
         {results ? <Results results={results} /> : ""}
       </div>
     );
