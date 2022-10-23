b07848f7322ef5bf0dbe782faae6b00f7517c40c
Evan Astle <starphys@gmail.com>
2022-10-23 14:23:50 -0700

    Added rating visualization (stars)

--- spartanstay/frontend/src/components/Listing.js
+++ spartanstay/frontend/src/components/Listing.js
@@ -1,11 +1,12 @@
+import StarRatings from "react-star-ratings"
 
 function Listing ({listing}) {
 //{listing.ratePlan.price.current}
     return (
         <div>
-            {listing.name} {listing.ratePlan ? listing.ratePlan.price.current : "Priceless"}<br />
             <img src={listing.optimizedThumbUrls ? listing.optimizedThumbUrls.srpDesktop : "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=250&h=140"} alt="img" /><br /> 
-            {listing.starRating}/5, {listing.address.streetAddress}, {listing.address.locality} {listing.address.region}, {listing.address.countryName} <br />  <br /> 
+            {listing.name}<br /> 
+            {listing.ratePlan ? listing.ratePlan.price.current : "Priceless"}<StarRatings rating={listing.starRating} starRatedColor="red" numberOfStars={5} starDimension="19px" starSpacing="0"/><br />  <br /> 
         </div>
     )
 
