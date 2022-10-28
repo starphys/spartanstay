07a19714df1e989bf16133f663b154b738a791cf
Evan Astle <starphys@gmail.com>
2022-10-28 01:43:03 -0700

    Integration to backend seems to be working

--- spartanstay/frontend/src/App.js
+++ spartanstay/frontend/src/App.js
@@ -3,10 +3,18 @@ import SignUp from "./pages/SignUp"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Payment from "./pages/Payment"
+import React, { useState } from 'react';
 import { Route, Routes } from "react-router-dom"
 
 
 function App() {
+  const [payment,setPayment] = useState('')
+  const token = {
+    id:1,
+    email:"email@website.com",
+    firstName:"Ian",
+    lastName:"van Dyke",
+  }
   return (
     <>
       <Navbar />
@@ -15,7 +23,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/about" element={<About />} />
-          <Route path="/payment" element={<Payment />} />
+          <Route path="/payment" element={<Payment token={token} setPayment={setPayment} />} />
         </Routes>
       </div>
     </>
--- spartanstay/frontend/src/pages/Payment.js
+++ spartanstay/frontend/src/pages/Payment.js
@@ -1,23 +1,67 @@
 
  import "../payment.css";
- import React, { Component } from 'react';
+ import React, { useState } from 'react';
  import PaymentIcon from 'react-payment-icons';
  import { FaUser } from 'react-icons/fa';
  import { MdEmail } from 'react-icons/md';
  import { FaAddressCard } from 'react-icons/fa';
  import { FaCity } from 'react-icons/fa';
 
+ function Payment({token, setPayment}) {
 
+  const [cardNum, setCardNum] = useState('')
+  const [expMonth, setExpMonth] = useState('')
+  const [expYear, setExpYear] = useState('')
+  const [securityCode, setSecurityCode] = useState('')
+  const [fName, setFName] = useState('')
+  const [address, setAddress] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [zip, setZip] = useState('')
+  const [savePayment, setSavePayment] = useState('')
 
+  const handleClick = (e) => {
+    e.preventDefault()
+    const payment = {
+      userId:token.id,
+      paymentType:"Credit",
+      cardNumber:cardNum,
+      expMonth:expMonth,
+      expYear:expYear,
+      securityCode:securityCode,
+      nameOnCard:fName,
+      billingName:fName,
+      billingAddress:address,
+      billingCity:city,
+      billingState:state,
+      billingZip:zip
+    }
+   
+    setPayment({
+      cardNum:cardNum,
+      expDate:expMonth + expYear,
+      securityCode:securityCode,
+      address:address
+    })
 
- function Payment() {
+    if(savePayment) {
+       fetch("http://localhost:8080/payments/addCardDetails", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payment)
+        }
+        ).then((response) => {
+            return response.json()
+        }).then((data) => {console.log(data) })
+    }
+  }
+
    return (
      <>
          <div class="fill" ><h1 class="neonText" >Payment Info</h1></div>
 
         <br></br>
         <div id="wrap">
-           <form action="/action_page.php">
              <header>
                <div class="billingAddress">
                  <br></br>
@@ -25,21 +69,21 @@
 
 
                <label for="fname"><pre><FaUser size={15}/>  Full Name </pre></label>
-              <input type="text" id="fname" name="firstname" placeholder="John M. Doe"></input>
+              <input type="text" id="fname" name="firstname" placeholder="John M. Doe" onChange={(e)=>setFName(e.target.value)}></input>
               <label for="email"><pre><MdEmail size={20}/>  Email </pre></label>
              <input type="text" id="email" name="email" placeholder="john@example.com"></input>
              <label for="adr"><pre><FaAddressCard size={20}/>  Address </pre></label>
-             <input type="text" id="adr" name="address" placeholder="542 W. 15th Street"></input>
+             <input type="text" id="adr" name="address" placeholder="542 W. 15th Street" onChange={(e) => setAddress(e.target.value)}></input>
              <label for="city"><pre><FaCity size={20}/>  City</pre></label>
-             <input type="text" id="city" name="city" placeholder="New York"></input>
+             <input type="text" id="city" name="city" placeholder="New York" onChange={(e)=>setCity(e.target.value)}></input>
              <div class="row">
                <div class="col-50">
                  <label for="state">State</label>
-                 <input type="text" id="state" name="state" placeholder="NY"></input>
+                 <input type="text" id="state" name="state" placeholder="NY" onChange={(e)=>setState(e.target.value)}></input>
                </div>
                <div class="col-50">
                  <label for="zip">Zip</label>
-                  <input type="text" id="zip" name="zip" placeholder="10001"></input>
+                  <input type="text" id="zip" name="zip" placeholder="10001" onChange={(e)=>setZip(e.target.value)}></input>
                 </div>
               </div>
                </div> 
@@ -64,40 +108,31 @@
               <label for="cname">Name on Card</label>
               <input type="text" id="cname" name="cardname" placeholder="John More Doe"></input>
               <label for="ccnum">Credit card number</label>
-              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"></input>
+              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" onChange={(e)=>setCardNum(e.target.value)}></input>
               <div class= "row">
 
                 <div class="expcvv">
                   <label for="expmonth">Exp Month</label>
-                  <input type="text" id="expmonth" name="expmonth" placeholder="September"></input>
+                  <input type="text" id="expmonth" name="expmonth" placeholder="September" onChange={(e) => setExpMonth(e.target.value)}></input>
                 </div>
                 <div class="expcvv">
                   <label for="expyear">Exp Year</label>
-                  <input type="text" id="expyear" name="expyear" placeholder="2018"></input>
+                  <input type="text" id="expyear" name="expyear" placeholder="2018" onChange={(e) => setExpYear(e.target.value)}></input>
                 </div>
                 <div class="expcvv">
                   <label for="cvv">CVV</label>
-                  <input type="text" id="cvv" name="cvv" placeholder="352"></input>
+                  <input type="text" id="cvv" name="cvv" placeholder="352" onChange={(e) => setSecurityCode(e.target.value)}></input>
                 </div>
               </div>
             </div>
 
           <div id="parent">
-          <input class="child saveInput" id="element1" type="checkbox" checked="checked" name="sameadr"></input><label id="element2"class="child save">Shipping address same as billing</label>
-          </div>
-          <br></br>
-
-          <div id="parent">
-            <input class="child saveInput" id="element1"  type="checkbox" checked="checked" name="sameadr"></input><label id="element2" class="child save"> Save payment method</label>
+            <input class="child saveInput" id="element1"  type="checkbox" name="sameadr" onChange={(e) =>setSavePayment(e.target.value)}></input><label id="element2" class="child save"> Save payment method</label>
             </div>
 
-          <input class="btn button_slide slide_right" type="submit" value="Continue to checkout"></input>
+          <input class="btn button_slide slide_right" type="button" value="Continue to checkout" onClick={handleClick}></input>
           </div>
-        </form>
         </div>
-
-
-
      </>
    );
  }
