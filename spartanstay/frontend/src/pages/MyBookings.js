16d9d7054f028082951e41683f1b7494a76a23d4
Evan Astle <starphys@gmail.com>
2022-11-08 16:44:14 -0800

    Successfully getting reservations in mybookings

--- spartanstay/frontend/src/pages/MyBookings.js
+++ spartanstay/frontend/src/pages/MyBookings.js
@@ -1,10 +1,15 @@
 
-function MyBookings({token}) {    
+function MyBookings({token}) {
+    fetch(`http://localhost:8080/reservation/mybookings?id=${token.id}`)
+    .then((response)=>{
+    return response.json()
+  }).then(data => { return data}).then(data => console.log(data))
+    
   return (
     <div className="">
         Hello {token.firstName}!
     </div>
-  );
+    );
 }
 
 export default MyBookings;
