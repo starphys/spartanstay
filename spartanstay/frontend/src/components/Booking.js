632d8cf42e3c905fc92cc16d7017fd4ed803ce10
BrendaCasas <brenda.casas@sjsu.edu>
2022-11-13 11:31:13 -0800

    Edit and delete buttons for bookings were added

--- spartanstay/frontend/src/components/Booking.js
+++ spartanstay/frontend/src/components/Booking.js
@@ -10,62 +10,63 @@ import "./Booking.css";
 
 function Booking({ booking }) {
     return (
-        <div id='resp-table'>
-            <div id='resp-table-header'>
-                <div class='table-header-cell'>
-                
+        <div> 
+            <div id='resp-table'>
+                <div id='resp-table-header'>
+                    <div class='table-header-cell'>
+                    
+                    </div>
+                    <div class='table-header-cell'>
+                    Hotel Name
+                    </div>
+                    <div class='table-header-cell'>
+                    Room Type
+                    </div>
+                    <div class='table-header-cell'>
+                    Check-In Date
+                    </div>
+                    <div class='table-header-cell'>
+                    Check-Out Date
+                    </div>
+                    <div class='table-header-cell'>
+                    Number of Adults
+                    </div>
+                    <div class='table-header-cell'>
+                    Number of Children
+                    </div>
+                    <div class='table-header-cell'>
+                    Special Request
+                    </div>
+                    <div class='table-header-cell'>
+                    Phone Number
+                    </div>
+                    <div class='table-header-cell'>
+                    Price
+                    </div>
+                    <div class='table-header-cell'>
+                    Confirmation Number
+                    </div>
                 </div>
-                <div class='table-header-cell'>
-                Hotel Name
-                </div>
-                <div class='table-header-cell'>
-                Room Type
-                </div>
-                <div class='table-header-cell'>
-                Check-In Date
-                </div>
-                <div class='table-header-cell'>
-                Check-Out Date
-                </div>
-                <div class='table-header-cell'>
-                Number of Adults
-                </div>
-                <div class='table-header-cell'>
-                Number of Children
-                </div>
-                <div class='table-header-cell'>
-                Special Request
-                </div>
-                <div class='table-header-cell'>
-                Phone Number
-                </div>
-                <div class='table-header-cell'>
-                Price
-                </div>
-                <div class='table-header-cell'>
-                Confirmation Number
+                <div id='resp-table-body'>
+                    <div class='resp-table-row'>
+                        <img class="imageChange, table-body-cell" src={booking.image} alt="img" />
+                        <div className='table-body-cell'>Hotel Name</div>
+                        <div className='table-body-cell'>{booking.roomType}</div>
+                        <div className='table-body-cell'>{booking.checkInDate}</div>
+                        <div className='table-body-cell'>{booking.checkOutDate}</div>
+                        <div className='table-body-cell'>{booking.numAdult}</div>
+                        <div className='table-body-cell'>{booking.numChildren}</div>
+                        <div className='table-body-cell'>{booking.specialReq}</div>
+                        <div className='table-body-cell'>{booking.phoneNum}</div>
+                        <div className='table-body-cell'>{booking.totalCost}</div>
+                        <div className='table-body-cell'>{booking.id}</div>
+                    </div>
                 </div>
             </div>
-            <div id='resp-table-body'>
-                <div class='resp-table-row'>
-                    <img class="imageChange, table-body-cell" src={booking.image} alt="img" />
-                    <div className='table-body-cell'>Hotel Name</div>
-                    <div className='table-body-cell'>{booking.roomType}</div>
-                    <div className='table-body-cell'>{booking.checkInDate}</div>
-                    <div className='table-body-cell'>{booking.checkOutDate}</div>
-                    <div className='table-body-cell'>{booking.numAdult}</div>
-                    <div className='table-body-cell'>{booking.numChildren}</div>
-                    <div className='table-body-cell'>{booking.specialReq}</div>
-                    <div className='table-body-cell'>{booking.phoneNum}</div>
-                    <div className='table-body-cell'>{booking.totalCost}</div>
-                    <div className='table-body-cell'>{booking.id}</div>
-                </div>
-            </div>
+            <button> Edit Reservation</button>
+            <button> Delete Reservation</button>
         </div>
-
     )
-    
-
 }
 
 export default Booking
